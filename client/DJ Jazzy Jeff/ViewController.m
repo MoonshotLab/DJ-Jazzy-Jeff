48a0e49706aec43dd32575e837f8a0dad326cb46
Joe Longstreet <joelongstreet@gmail.com>
2014-11-05 13:06:37 -0600

    post user name to web service

--- client/DJ Jazzy Jeff/ViewController.m
+++ client/DJ Jazzy Jeff/ViewController.m
@@ -13,6 +13,7 @@
 
 @interface ViewController () <INBeaconServiceDelegate, MFMailComposeViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *userNameView;
+@property NSString *userName;
 
 
 @end
@@ -23,29 +24,58 @@
     [super viewDidLoad];
 
     NSUserDefaults *userData = [NSUserDefaults standardUserDefaults];
-    NSString *userName = [userData objectForKey:@"userName"];
+    NSString *storedUserName = [userData objectForKey:@"userName"];
     
-    if(![userName length]){
-        userName = [[NSUUID UUID] UUIDString];
-        [userData setValue:userName forKey:@"userName"];
+    if(![storedUserName length]){
+        _userName = [[NSUUID UUID] UUIDString];
+        [userData setValue:_userName forKey:@"userName"];
         [userData synchronize];
-    }
-    
-    NSMutableString *usernameText = [NSMutableString string];
-    [usernameText appendString:@"username :"];
-    [usernameText appendString:userName];
-    self.userNameView.text = usernameText;
+        
+        [self registerUser:_userName];
+    } else
+        [self showUserName];
     
     INBeaconService *beaconService = [[INBeaconService alloc] initWithIdentifier:@"CB284D88-5317-4FB4-9621-C5A3A49E6155"];
 
-    [beaconService setUserName:userName];
+    [beaconService setUserName:_userName];
     [beaconService addDelegate:self];
     [beaconService startBroadcasting];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)connection:(NSURLConnection *) connection didReceiveData:(NSData *)data{
+    [self showUserName];
+}
+
+- (void)connection:(NSURLConnection *) connection didFailWithError:(NSError *)error{
+    NSLog(error);
+}
+
+- (void) showUserName {
+    NSMutableString *userNameText = [NSMutableString string];
+    [userNameText appendString:@"username :"];
+    [userNameText appendString:_userName];
+}
+
+- (BOOL)registerUser:(NSString *)userName {
+    
+    NSString *postString = [NSString stringWithFormat:@"username=%@", @"username"];
+    NSData *postData = [postString dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+    NSMutableURLRequest *request = [NSMutableURLRequest alloc];
+
+    [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://localhost:3000/user/create"]]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
+    
+    NSURLConnection *urlConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    if(urlConnection)
+        NSLog(@"Connection Made");
+    else
+        NSLog(@"Connection failed");
+    
+    return true;
 }
 
 @end
